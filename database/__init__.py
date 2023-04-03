152b9752e02ef834c123f1792251ce2cdc1d10b0
Potato Mexicano <potatomexicano006@yahoo.com>
2023-04-03 14:54:00 -0300

    Finish connection

--- database/__init__.py
+++ database/__init__.py
@@ -1,27 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import URL
+from decouple import config
 
-database = URL.create(
-    drivername='postgresql+psycopg2',
-    username='postgres',
-    password='root',
-    host='192.168.245.212',
-    port='5432',
-    database='rastreador-de-veiculos'
-)
-
+# DEVELOPMENT
 # database = URL.create(
-#     drivername='mssql+pyodbc',
-#     username='montadroid',
-#     password='md.002#',
-#     host='192.168.245.17',
-#     port='1433',
-#     database='RastreadorVeicular',
-#     query={
-#         "driver": "SQL Server Native Client 11.0",
-#     },
+#     drivername='postgresql+psycopg2',
+#     username= config('DEV_DB_USER'),
+#     password= config('DEV_DB_PWD'),
+#     host= config('DEV_DB_HOST'),
+#     port= config('DEV_DB_PORT'),
+#     database= config('DEV_DB_DATABASE')
 # )
+
+# PRODUCTION
+database = URL.create(
+    drivername='mssql+pyodbc',
+    username= config('DB_USER'),
+    password= config('DB_PWD'),
+    host= config('DB_HOST'),
+    port= config('DB_PORT'),
+    database= config('DB_DATABASE'),
+    query={
+        "driver": "SQL Server Native Client 11.0",
+    },
+)
 
 engine = create_engine(database)
 
